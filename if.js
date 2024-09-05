e934ddb89f4acf1d50dcb3902464cca3315b33cb
bd-web-dev <isnain.cmd@outlook.com>
2024-09-06 05:01:57 +0600

    Module 18 Video 4: If else conditional statements.

---
+++ if.js
@@ -0,0 +1,41 @@
+// Tui jodi amake khawash taile ami ashbo
+// If you give me a treat, I will meet up
+// If biryani price less than 300tk, I will buy
+
+// STRUCTURE of conditionals
+// if (condition) {
+//      do this;
+// }
+
+if (3 < 10) {
+  console.log("3 is less than 10.");
+}
+
+if (13 < 10) {
+  console.log("13 is less than 10.");
+}
+
+var biryaniPrice = 500;
+if (biryaniPrice < 300) {
+  console.log("Mama give me some biryani.");
+}
+
+biryaniPrice = 250;
+if (biryaniPrice < 300) {
+  console.log("Mama give me some biryani now.");
+}
+
+// if (condition) {
+//     execute code if condition above is true;
+// } else {
+//     execute code if condition above is false;
+// }
+
+var weight = 40;
+// weight = 10;
+weight = 20;
+if (weight < 20) {
+  console.log("I will carry it by myself.");
+} else {
+  console.log("I will rent a rickshaw.");
+}
