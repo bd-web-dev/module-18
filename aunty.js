b4a507803bb0b30bb2862d1c770111f65124ee1f
bd-web-dev <isnain.cmd@outlook.com>
2024-09-06 00:06:22 +0600

    Module 18 Video 1: Recap of variables and detailed discussion of comparison operators in JS.

---
+++ aunty.js
@@ -0,0 +1,11 @@
+// Different types of comparisons
+// 1. Greater than              >
+// 2. Less than                 <
+// 3. Equal to                  ==
+// 4. Greater than or equal to  >=
+// 5. Less than or equal to     <=
+// 6. Not equal to              !=
+
+// -------------------------------
+// 7. AND operator:             &&
+// 8. OR operator:              ||
---
+++ variable.js
@@ -0,0 +1,45 @@
+// Variables
+// Variables are containers holding values.
+// The values may vary.
+// There are 5 components of a variable
+// 1. var keyword
+// 2. Variable name
+// 3. Assignment operator
+// 4. Value to be assigned to variable
+// 5. Semi-colon
+var sunglassPrice = 80;
+
+// Types of variables
+// Number data type
+var number = 2024;
+// String data type
+var subject = "EEE";
+// Boolean data type
+var passed = false;
+
+// Naming Conventions
+// 1. Variable names must be a single word
+// 2. No quotes in variable name
+// 3. No space or hyphen in variable name
+// 4. Can not start with digit
+// 5. Can not be a keyword or reserved word
+// 6. Multiple cases for long names. Preferred is camelCase
+// camelCase variable name
+var lastExamResult = 89;
+
+// Operations
+// 1. Add +
+// 2. Subtract -
+// 3. Multiply *
+// 4. Divide /
+// 5. Modulo operator %
+
+// Shorthands
+// 1. +=
+// 2. -=
+// 3. *=
+// 4. /=
+
+// 1. Fucntion of isNaN
+// 2. Definition of null
+// 3. Number
