763ed98cd93e9708ab20d723f66a091eae379a68
bd-web-dev <isnain.cmd@outlook.com>
2024-09-07 06:43:10 +0600

    Module 18 Video 7: Nested if else statements.

---
+++ nested-if-else.js
@@ -0,0 +1,63 @@
+let money = 500;
+console.log("Run 1");
+if (money > 300) {
+  console.log("Bro you are rich.");
+} else {
+  if (money > 100) {
+    console.log("Tui gorib o na, borolok o na.");
+  } else {
+    if (money > 0) {
+      console.log("Dosto ruti diye kola kha. Calcium bara.");
+    } else {
+      console.log("Tui amar bondhu na.");
+    }
+  }
+}
+
+money = 200;
+console.log("Run 2");
+if (money > 300) {
+  console.log("Bro you are rich.");
+} else {
+  if (money > 100) {
+    console.log("Tui gorib o na, borolok o na.");
+  } else {
+    if (money > 0) {
+      console.log("Dosto ruti diye kola kha. Calcium bara.");
+    } else {
+      console.log("Tui amar bondhu na.");
+    }
+  }
+}
+
+money = 20;
+console.log("Run 3");
+if (money > 300) {
+  console.log("Bro you are rich.");
+} else {
+  if (money > 100) {
+    console.log("Tui gorib o na, borolok o na.");
+  } else {
+    if (money > 0) {
+      console.log("Dosto ruti diye kola kha. Calcium bara.");
+    } else {
+      console.log("Tui amar bondhu na.");
+    }
+  }
+}
+
+money = -10;
+console.log("Run 4");
+if (money > 300) {
+  console.log("Bro you are rich.");
+} else {
+  if (money > 100) {
+    console.log("Tui gorib o na, borolok o na.");
+  } else {
+    if (money > 0) {
+      console.log("Dosto ruti diye kola kha. Calcium bara.");
+    } else {
+      console.log("Tui amar bondhu na.");
+    }
+  }
+}
