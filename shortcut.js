57ee4a488f66615e2c87f7d540f6aa4d965c72f3
bd-web-dev <isnain.cmd@outlook.com>
2024-09-08 07:20:40 +0600

    Module 18 Video 9: Logical NOT operator to simplify checking of truthy and falsy values. Not limited to Boolean values, includes Number and String as well.

---
+++ shortcut.js
@@ -0,0 +1,24 @@
+const isLeader = true;
+// if (isLeader === true) {
+//   console.log("Leader ashtese. Chair char.");
+// } else {
+//   console.log("Bhitore dhukte dibi na.");
+// }
+
+if (isLeader) {
+  console.log("Leader ashtese. Chair char.");
+} else {
+  console.log("Bhitore dhukte dibi na.");
+}
+
+const isPassed = false;
+// if (isPassed === false) {
+//   console.log("Tore biye diye dibo.");
+// } else {
+//   console.log("Good job.");
+// }
+if (!isPassed) {
+  console.log("Tore biye diye dibo.");
+} else {
+  console.log("Good job.");
+}
